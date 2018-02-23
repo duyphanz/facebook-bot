08bcabd0b2c3b875c4e04d37a58e9fd0593cbe49
DuyPhan <phanduy344@gmail.com>
2018-02-23 09:53:22 +0700

    fb-verify

---
+++ app_server/routes/facebook-bot.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+
+//authen facebook
+router.get('fb-bot', (req, res) => {
+    
+
+// Your verify token. Should be a random string.
+let VERIFY_TOKEN = "ThisIsSecret"
+
+ // Parse the query params
+ let mode = req.query['hub.mode'];
+ let token = req.query['hub.verify_token'];
+ let challenge = req.query['hub.challenge'];
+ console.log(req.query);
+   
+ // Checks if a token and mode is in the query string of the request
+ if (mode && token) {
+ 
+   // Checks the mode and token sent is correct
+   if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+     
+     // Responds with the challenge token from the request
+     console.log('WEBHOOK_VERIFIED');
+     res.status(200).send(challenge);
+   
+   } else {
+     // Responds with '403 Forbidden' if verify tokens do not match
+     res.sendStatus(403);      
+   }
+ }
+
+})
