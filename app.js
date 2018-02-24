399ca1e4ee2e8089755e9c26322d92e5e4cd3f35
DuyPhan <phanduy344@gmail.com>
2018-02-24 10:02:14 +0700

    mongoose find func

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphdbs = require('express-handlebars');
+//require setup mongoose db
 require('./app_server/models/db');
 
+// require routes
 var index = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
 var fb = require('./app_server/routes/facebookbot');
@@ -14,7 +17,14 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '/app_server/views'));
-app.set('view engine', 'jade');
+var hbs = exphdbs.create({
+  defaultLayout: null,
+  layoutsDir: 'app_server/views/layouts'
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- app_server/controllers/ctrlIndex.js
+++ app_server/controllers/ctrlIndex.js
@@ -1,8 +1,18 @@
+var mongoose = require('mongoose');
+var Link = mongoose.model('Link')
+
 
 module.exports.home = function (req, res) {
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-    console.log(req.query);
-    res.render('index', {title : 'Express'});
+    getLinkData(res);
+}
+
+function getLinkData(res) {
+    Link.find({}, (err, links) => {
+        if(err) {
+            console.log('****Loi get link data');
+            res.render('error');
+        }
+        console.log(links)
+        res.render('index', {links: links});
+    })
 }
