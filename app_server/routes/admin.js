d028a7fa660087a4b9101bb1612decbc7e043e7d
DuyPhan <phanduy344@gmail.com>
2018-03-21 23:29:03 +0700

    admin page

--- app_server/routes/admin.js
+++ app_server/routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {linkApprove, approveLink} = require('../controllers/ctrlAdmin')
+const {linkApprove, approveLink, rejectLink} = require('../controllers/ctrlAdmin')
 const {isAdmin} = require('../controllers/ctrlAuth')
 
 router.get('/linkApprove', isAdmin, linkApprove)
 router.get('/accept/:id/:bot',isAdmin , approveLink )
+router.get('/reject/:id/:bot',isAdmin , rejectLink )
 
 module.exports = router;
